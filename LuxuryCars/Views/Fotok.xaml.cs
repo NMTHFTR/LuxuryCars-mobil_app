fad68fe4697c0e1ac0d927a62261d96f60d14b62
NMTHFTR <benedekosze@gmail.com>
2024-04-15 13:40:58 +0200

    aaaa

--- LuxuryCars/Views/Fotok.xaml.cs
+++ LuxuryCars/Views/Fotok.xaml.cs
@@ -1,13 +1,16 @@
-
+using System.Collections.ObjectModel;
 
 namespace LuxuryCars.Views;
 
 
 public partial class Fotok : ContentPage
 {
+
 	public Fotok()
 	{
 		InitializeComponent();
+
+	
 
 	}
 }
