d5ea8d5c5ce1efb13b2f2933ab16101b1224ab85
NMTHFTR <benedekosze@gmail.com>
2024-04-15 22:18:08 +0200

    kepek page

---
+++ LuxuryCars/Models/CollectionItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxuryCars.Models
+{
+    public class CollectionItem
+    {
+        public string title { get; set; }
+    }
+}
--- LuxuryCars/Views/Fotok.xaml.cs
+++ LuxuryCars/Views/Fotok.xaml.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using LuxuryCars.Models;
 using System.Collections.ObjectModel;
 
 namespace LuxuryCars.Views;
@@ -5,12 +7,15 @@ namespace LuxuryCars.Views;
 
 public partial class Fotok : ContentPage
 {
-
-	public Fotok()
+    
+    public Fotok()
 	{
 		InitializeComponent();
+        
+          var items=new List<CollectionItem> { 
+            new CollectionItem{title="embed://911_GT3.mp4"},
+        };
+        CaruselView.ItemsSource = items;
 
-	
-
-	}
+    }
 }
