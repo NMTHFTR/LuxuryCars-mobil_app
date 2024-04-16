db02e151c9a3b2cad1ca370b2c5ebe40bda575c3
NMTHFTR <benedekosze@gmail.com>
2024-04-16 11:41:08 +0200

    fotokpage

--- LuxuryCars/Views/Fotok.xaml.cs
+++ LuxuryCars/Views/Fotok.xaml.cs
@@ -13,9 +13,13 @@ public partial class Fotok : ContentPage
 		InitializeComponent();
         
           var items=new List<CollectionItem> { 
-            new CollectionItem{title="embed://911_GT3.mp4"},
+            new CollectionItem{title="https://s1.cdn.autoevolution.com/images/gallery/BENTLEY-Continental-GTC-6460_13.jpg"},
+            new CollectionItem{title="https://s1.cdn.autoevolution.com/images/gallery/ROLLS-ROYCE-Wraith-4791_29.jpg"},
+            new CollectionItem{title="https://s1.cdn.autoevolution.com/images/gallery/NISSAN-GT-R-5603_18.jpg"},
+            new CollectionItem{title="https://s1.cdn.autoevolution.com/images/gallery/BUGATTI-Chiron-5583_35.jpg"},
+            new CollectionItem{title="https://s1.cdn.autoevolution.com/images/gallery/DODGE-Challenger-SRT-Demon-5978_19.jpg"},
         };
         CaruselView.ItemsSource = items;
-
+        
     }
 }
